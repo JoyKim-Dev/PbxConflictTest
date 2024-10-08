2dccd5d16c8a2c284d378c70abc107c61e8d4e4f
Joy <kimjoydev@gmail.com>
2024-10-08 11:13:54 +0900

    Initial Commit

---
+++ PbxConflictTest/PbxConflictTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PbxConflictTestApp.swift
+//  PbxConflictTest
+//
+//  Created by Joy Kim on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PbxConflictTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
